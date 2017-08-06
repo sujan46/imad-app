d0451852d77975c9cbe238671144c8ab85dbbff7
sujan46 <sujanrao46@gmail.com>
2017-08-06 16:55:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ app.get('/article1',function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article1.html'));   
 });
 
-app.get('/article1',function (req, res) {
+app.get('/article2',function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article2.html'));   
 });
 
-app.get('/article1',function (req, res) {
+app.get('/article3',function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article3.html'));   
 });
 
